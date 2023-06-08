08676ae061a7506b1a3bf99bd8a8a4a868fbc12e
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-08 19:19:49 +0200

    more on the intro slide

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "beautiful-react-diagrams/styles.css";
+import "./templates/main.css"
 import PresentationDeck from "./presentation";
 
 
