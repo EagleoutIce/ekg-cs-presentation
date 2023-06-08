c760fa29119943faaddca1384b645c69a45694ce
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-08 23:28:50 +0200

    basic diagram huge effort

--- src/presentation/segments/cryptography.tsx
+++ src/presentation/segments/cryptography.tsx
@@ -1,54 +1,96 @@
-import { Appear, Text, Slide, Notes, Image } from "spectacle";
+import { Appear, Text, Slide, Notes, Image, Stepper } from "spectacle";
 import { CenterOnSlide,  LargeWaveText,  RawText,  References, WaveText } from "../../templates/styles";
+import ReactFlow, {
+   MiniMap,
+   Controls,
+   Background,
+   useNodesState,
+   useEdgesState,
+   addEdge,
+   Handle,
+   Position,
+ } from 'reactflow';
+
+import 'reactflow/dist/style.css';
+import { useMemo } from "react";
+import { penguinA, penguinB } from "../../penguins";
 import { ekgTheme } from "../../settings";
-import { penguinA } from "../../penguins";
-
-import { CanvasWidget } from '@projectstorm/react-canvas-core';
-import createEngine, { DiagramModel, DefaultNodeModel, DefaultLinkModel, DefaultLabelModel } from '@projectstorm/react-diagrams';
 
 
-const Fun = () => {
-	const engine = createEngine({
-      /* i dislike zoom for this initial graphic - this could happen accidental */
-		registerDefaultPanAndZoomCanvasAction: false,
-      registerDefaultZoomCanvasAction: false
-	});
+interface ImageNodeData {
+   type: 'in' | 'out' | 'both'
+   source: string
+   label: string
+}
 
-   // node 1
-   const node1 = new DefaultNodeModel({
-      name: 'Node 1'
-   });
-   node1.setPosition(100, 100);
-   let port1 = node1.addOutPort('Out');
-   port1.setLocked(true);
+// TODO: collision detection
+function MainImageNode(input: { data: ImageNodeData }) {
+   return (
+     <>
+       { input.data.type !== 'out' && <Handle type="target" position={Position.Left} isConnectableStart={false} isConnectableEnd={false} isConnectable={false} style={{ marginRight: '10px' }} />}
+       <img src={input.data.source} style={{ height: 100 }} />
+       <div style={{textAlign: 'center', fontFamily: ekgTheme.fonts.header, marginTop: '5px' }}>{input.data.label}</div>
+       { input.data.type !== 'in' && <Handle type="source" position={Position.Right} isConnectableStart={false} isConnectableEnd={false} isConnectable={false} style={{ marginLeft: '10px' }} /> }
+     </>
+   );
+ }
 
-   // node 2
-   const node2 = new DefaultNodeModel({
-      name: 'Node 1'
-   });
-   node2.setPosition(600, 100);
-   let port2 = node2.addInPort('In');
+ const initialNodes = [
+   { id: '1', type: 'image', position: { x: 0, y: 0 }, data: { source: penguinA, type: 'out', label: "Alice" } },
+   { id: '2', type: 'image', position: { x: 500, y: 0 }, data: { source: penguinB, type: 'in', label: "Bob" } },
+ ];
 
-   port2.setLocked(true);
+ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-   const link = port1.link<DefaultLinkModel>(port2);
-   link.setLocked(true);
+ const defaultEdgeOptions = {
+   animated: true
+ };
+ const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
 
-   const model = new DiagramModel();
-   model.addAll(node1, node2, link);
-   engine.setModel(model);
+const PenguinsCommunicate: React.FC = () => {
+   const [nodes, _setNodes, onNodesChange] = useNodesState(initialNodes);
+   const [edges, _setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-	return (
-      <div style={{ height:"50%", marginTop: '12.5%' }}>
-		   <CanvasWidget engine={engine} className="diagram-canvas"/>
-      </div>
-	);
-};
+   const nodeTypes = useMemo(() => ({ image: MainImageNode }), []);
+
+   return (
+     <ReactFlow
+       nodeTypes={nodeTypes}
+       nodes={nodes}
+       edges={edges}
+       onNodesChange={onNodesChange}
+       onEdgesChange={onEdgesChange}
+       defaultEdgeOptions={defaultEdgeOptions}
+       snapGrid={[25, 25]}
+       defaultViewport={defaultViewport}
+       snapToGrid={true}
+       fitView
+       minZoom={0.5}
+       maxZoom={2}
+       edgesUpdatable={false}
+       nodesConnectable={false}
+       nodesDraggable={true}
+       edgesFocusable={false}
+       proOptions={{ hideAttribution: true }}
+       style={{
+         padding: 0,
+         margin: 0
+       }}
+     >
+     </ReactFlow>
+   );
+}
 
 export const Cryptography: React.FC = () => {
-   return (<Slide>
-      <Fun />
-      {/* <Image src={penguinA} /> */}
+
+   return (<Slide padding={'0px'} >
+      <CenterOnSlide>
+      <div style={{ width: "100%", height: "100%" }}>
+         <Appear>
+               <PenguinsCommunicate />
+         </Appear>
+      </div>
+      </CenterOnSlide>
     <Notes>
       TODO
    </Notes>
