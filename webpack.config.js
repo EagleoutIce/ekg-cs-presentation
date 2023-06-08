ee8511a5df39be50a378c937fda2ecabd3b607d0
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-08 15:01:04 +0200

    clean up deployment

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.tsx',
+	devServer: {
+		static: path.join(__dirname, 'dist'),
+		host: '0.0.0.0',
+		port: 3000
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Von Physik, Informatik und der Welt',
+			favicon: 'public/favicon.ico'
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|pdf)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[path][name].[ext]',
+						esModule: false,
+					}
+				}]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
+			}
+		]
+	},
+	resolve: {
+		extensions: [ '.ts', '.tsx', '.js']
+	},
+	performance: {
+		hints: "warning"
+	},
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true
+	}
+};
