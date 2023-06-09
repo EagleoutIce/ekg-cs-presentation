c5fdceb882aa09c6378ba0d81bc39ce701239e09
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-09 12:18:32 +0200

    scale github link on hover

--- src/templates/title.tsx
+++ src/templates/title.tsx
@@ -14,7 +14,7 @@ const TitleSlide: React.FC<TitleSlideInputs> = ({ title }) => (
       <LargeWaveText text={title} svgHeight="150px" amplitude={40} points={15}/>
     </CenterOnSlide>
     <TopRight>
-      <a href="https://github.com/EagleoutIce/ekg-cs-presentation">
+      <a href="https://github.com/EagleoutIce/ekg-cs-presentation" className="scale-on-hover">
         <img src={gitHubLogo} width={32} height={32}/>
       </a>
     </TopRight>
