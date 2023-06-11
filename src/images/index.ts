555c16851c42f821f3f1b287b4b79df8bded4710
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-11 15:16:36 +0200

    back to tex

--- src/images/index.ts
+++
@@ -1,4 +0,0 @@
-export * as penguinA from '../../tex/penguins/Pingu-A.png'
-export * as penguinB from '../../tex/penguins/Pingu-B.png'
-export * as penguinE from '../../tex/penguins/Pingu-E.png'
-export * as gitHubLogo from '../../public/github-mark.png'
